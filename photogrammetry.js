96cb13bb92582569e21f9732199c3c2499b2c8fc
Marc LAMY <marclamy.bzh@gmail.com>
2021-02-19 17:09:21 +0100

    added Photosgrammetry mock file

--- photogrammetry.js
+++ photogrammetry.js
@@ -3,38 +3,20 @@ const casual = require('casual')
 const jsonServer = require('json-server')
 const server = jsonServer.create()
 const middlewares = jsonServer.defaults()
-const port = 7880
+const port = 7879
 
 server.use(jsonServer.bodyParser)
 server.use(middlewares)
 
-function generate_photos(min, max) {
-    let photos = []
-    if (min < 0 || max < min) {
-        return photos
-    }
-
-    min = Math.ceil(min)
-    max = Math.floor(max)
-    photos_count = Math.floor(Math.random() * (max - min + 1)) + min;
-
-    for (let i = 0; i < photos_count; i++) {
-        photos.push(casual.domain + '/' + casual.word + '.jpeg')
-    }
-
-    return photos
-}
 
 casual.register_provider({
-    submission: () => {
+    photogrammetry_job: () => {
         return {
-            id: casual.integer(from = 1, to = 256),
-            photos: generate_photos(8, 64),
-            submission_date: casual.date(format = 'YYYY-MM-DD') + 'T' + casual.time(format = 'hh:mm:ss.sssZ'),
         }
     }
 })
 
+/*
 server.get('/new_submissions', (request, response) => {
     if (request.method === 'GET') {
         const submissions = []
@@ -57,8 +39,8 @@ server.post('/change_submission_status', (request, response) => {
             response.status(404).jsonp()
         }
     }
-});
+});*/
 
 server.listen(port, () => {
-    console.log('Image storage API mock is running')
+    console.log('Photogrammetry API mock is running')
 })
