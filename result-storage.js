ab7175d940faa13576ddb046353cf54d57b347cf
Marc LAMY <marclamy.bzh@gmail.com>
2021-02-22 10:51:20 +0100

    Created result storage mock

---
+++ result-storage.js
@@ -0,0 +1,25 @@
+const casual = require('casual')
+
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const middlewares = jsonServer.defaults()
+const port = 7881
+
+server.use(jsonServer.bodyParser)
+server.use(middlewares)
+
+server.post('/result', (request, response) => {
+    if (request.method === 'POST') {
+        let submission_id = request.body['submission_id']
+        let result_url = request.body['result_url']
+        if (submission_id != null && result_url != null) {
+            response.status(200).jsonp()
+        } else {
+            response.status(404).jsonp()
+        }
+    }
+});
+
+server.listen(port, () => {
+    console.log('Result storage API mock is running')
+})
